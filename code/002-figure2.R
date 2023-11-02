97fe1da65bdd457e368c67e32a27eb52306d081b
zuckerm <62308716+zuckerm@users.noreply.github.com>
2023-11-02 18:02:04 -0400

    Add files via upload

--- code/002-figure2.R
+++ code/002-figure2.R
@@ -1,4 +1,4 @@
-biallelicRateTileplot <- function(data, xsize=14, mainsize=14, colorvec, subtypeTable, ncolors=4, maxAltRate=NULL){
+biallelicRateTileplot <- function(data, colorvec, subtypeTable, ncolors=4, xsize=14, mainsize=14){
   dat <- data
   #% rather than fraction:
   dat$`Biallelic %` <- round(100*(dat$`Biallelic Fraction`))
@@ -7,21 +7,14 @@ biallelicRateTileplot <- function(data, xsize=14, mainsize=14, colorvec, subtype
   ###Adding number of samples and disease abbrev. + n to be x-axis values:
   dat$n <- subtypeTable$n[match(dat$Disease, subtypeTable$Disease)]
   dat$abbrev <- subtypeTable$CTD_ABBR[match(dat$Disease, subtypeTable$CANCER_TYPE_DETAILED)]
-  dat$abbrev_n <- str_c(dat$abbrev, '_', dat$n)
-  
-  #Adding line breaks in disease names:
-  dat$abbrev_n <- sapply(dat$abbrev_n, function(d){
-    str <- strsplit(as.character(d), split = " ")[[1]]
-    chars <- length(strsplit(as.character(d), split = '')[[1]])
-    if(chars >= 30){
-      midpoint <- round(length(str)/2)
-      paste(paste(str[1:midpoint],collapse=" "), "\n", paste(str[(midpoint + 1):length(str)],collapse=" "),sep='')
-    }else{paste(str, collapse = " ")}
-  })
+  dat$abbrev_n <- str_c(dat$abbrev, ' (', dat$n, ')')
   
   #Gene-level object to be used to make barplot:
   genedf <- merge(unique(mechdf[,c('Hugo_Symbol','Pathway','gene_alts')]), dat[which(dat$Disease == 'all'),], by.x='Hugo_Symbol', by.y='Gene')
   genedf$`Alteration %` <- 100*genedf$Alteration_Rate
+  
+  #Adding gene_alt category to 'dat' for plotting purposes:
+  dat$gene_alts <- mechdf$gene_alts[match(dat$Gene, mechdf$Hugo_Symbol)]
 
   #Adding pathway info to inputs:
   dat$Pathway <- factor(mechdf$Pathway[match(dat$Gene, mechdf$Hugo_Symbol)], levels = levels(mechdf$Pathway))
@@ -30,6 +23,7 @@ biallelicRateTileplot <- function(data, xsize=14, mainsize=14, colorvec, subtype
   #Ordering organs:
   organOrder <- unique(subtypeTable$organ[order(subtypeTable$organ_order)])
   dat$organ <- factor(subtypeTable$organ[match(dat$Disease, subtypeTable$CANCER_TYPE_DETAILED)], levels = organOrder)
+  dat$organ_color <- as.character(subtypeTable$organ_color[match(dat$Disease, subtypeTable$CANCER_TYPE_DETAILED)])
   
   #Making sure abbrev_n levels are in right order:
   dat$abbrev_n <- factor(dat$abbrev_n, levels = unique(dat$abbrev_n))
@@ -45,8 +39,9 @@ biallelicRateTileplot <- function(data, xsize=14, mainsize=14, colorvec, subtype
   dat$`Alteration %`[which(dat$`Alteration %` == 0)] <- ''
   dat$`Biallelic %`[which(dat$`Alteration %` == 0)] <- 0
   dat$`Biallelic %`[which(dat$Alterations < 4)] <- 0
-  dat$`Alteration %`[which(dat$Alterations < 4)] <- NA
+  #dat$`Alteration %`[which(dat$Alterations < 4)] <- NA
   dat$`Biallelic %`[which(dat$`Alteration %` == '')] <- 0
+  dat$`Alteration %`[which(dat$Num_Alterations < 4 & dat$Num_Alterations > 0 | dat$`Alteration %` == '')] <- ''
   
   #Color gradient:
   colfunc <- colorRampPalette(c("white", "yellow", "red"))
@@ -57,10 +52,6 @@ biallelicRateTileplot <- function(data, xsize=14, mainsize=14, colorvec, subtype
   
   colfunc <- colorRampPalette(c("white", "#FFFB66", "#EF273E"))
   grad <- colfunc(ncolors)
-  
-  #Adding number of driver alterations in parentheses; because of facets_grid, have to add it to all three data frames and 
-  #make gene_alts the x-variable instead of gene; also have to make sure order is preserved:
-  dat$gene_alts <- factor(genedf$gene_alts[match(dat$Gene, genedf$Hugo_Symbol)], levels = unique(genedf$gene_alts))
   
   #Max alt rate plotted: 0.15
   genedf$Alteration_Rate[which(genedf$Alteration_Rate >= 0.15)] <- 0.15
@@ -75,13 +66,47 @@ biallelicRateTileplot <- function(data, xsize=14, mainsize=14, colorvec, subtype
       size = 6),
       legend.position = 'none')
   
+  #X axis text has to be aded manually:
+  lastGene <- head(genedf[order(genedf$Pathway, decreasing = TRUE),]$Hugo_Symbol, 1)
+  dat$xlabel <- ''
+  dat$xlabel[which(dat$Gene == lastGene)] <- dat$abbrev_n[which(dat$Gene == lastGene)]
+  
+  #Getting rid of any genes we don't have pathway assignement for:
+  dat <- dat[which(!is.na(dat$Pathway)),]
+  
   p_heatmap <- ggplot(data=dat,aes(x=abbrev_n,y=gene_alts,fill=`Biallelic %`)) + geom_tile() + theme_bw() + 
-    theme(axis.text.x = element_blank(),axis.text.y = element_blank(),axis.title.y = element_blank(), plot.title = element_text(size=mainsize),
+    theme(axis.text.x = element_blank(), axis.text.y = element_blank(),axis.title.y = element_blank(), plot.title = element_text(size=mainsize),
           legend.position = 'none') + 
+    theme(plot.margin = unit(c(1,1,9.7,1.5), "lines")) +
     geom_text(aes(label = `Alteration %`), color = "black", size = 3) + scale_fill_gradientn(colours = grad,values = seq(from=0, to=1, length=ncolors)) + 
     labs(x='',y='Gene') + ylab('') + ggtitle('') + 
     geom_point(data=dat[which(dat$Num_Alterations < 4 & dat$Num_Alterations > 0 | dat$`Alteration %` == ''),], shape=5, aes(x=abbrev_n,y=gene_alts,fill=`Biallelic %`)) + 
-    facet_grid(Pathway ~ organ, scales = "free", space = "free")
+    facet_grid(Pathway ~ organ, scales = "free", space = "free") + 
+    theme(strip.text = element_text(size = 6)) +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text.x = element_blank(),
+          strip.background = element_blank())
+  
+  tmb <- ggplot(data=fga_and_tmb_dat) + geom_violin(aes(x=abbrev_n, y=TMB), fill='#a0bcbb') + theme(axis.text.x = element_blank(),axis.text.y = element_text(size = xsize),axis.title.y = element_text(size = xsize),
+                                                                                                    axis.title.x = element_blank()) + xlab('Disease') + 
+    geom_text(data=fga_and_tmb_dat,aes(x=abbrev_n,y=-1.5,colour=organ_color,label=abbrev_n),angle=90,size=4,vjust=.5,hjust=1) + theme(legend.position='none') +
+    coord_cartesian(ylim=c(0,20), clip = 'off') + theme(plot.margin = unit(c(1,1,9.7,1.5), "lines")) + 
+    facet_grid(. ~ organ, scales = "free", space = "free") + 
+    theme(strip.text = element_text(size = 6)) +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text.x = element_blank(),
+          strip.background = element_blank()) 
+  
+  fga <- ggplot() + geom_violin(data=fga_and_tmb_dat, aes(x=abbrev_n, y=FGA),fill='#a492c5') + theme(axis.text.x = element_blank(),axis.title.x = element_blank(),
+                                                                                                     axis.text.y = element_text(size = xsize),axis.title.y = element_text(size = xsize)) + 
+    facet_grid(. ~ organ, scales = "free", space = "free")+ 
+    theme(strip.text = element_text(size = 6)) +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text.x = element_blank(),
+          strip.background = element_blank()) 
   
   p_bi <- ggplot(aes(x=gene_alts,y=Fraction_of_Alterations, fill=Mechanism), data=mechdf) + geom_bar(stat = 'identity',position = 'stack') + 
     coord_flip() + theme_bw() + xlab('') + ylab('Biallelic Fraction') + 
@@ -112,26 +137,21 @@ biallelicRateTileplot <- function(data, xsize=14, mainsize=14, colorvec, subtype
   layout <- 
     "
   ABCCCCD
+  ##EEEE#
+  ##FFFF#
+  ##GGGG#
   "
-  p <- p_alt + p_bi + p_heatmap + p_subtypes + plot_layout(design = layout, widths = c(.7,.7,.7,9), heights = c(8,.5,.5))
+  p <- p_alt + p_bi + p_heatmap + p_subtypes + plot_spacer() + fga + tmb + plot_layout(design = layout, widths = c(.7,.7,.7,9), heights = c(8,-1.63,.5,.5))
   p
 }
 
 #Generating figure 1:
-generateFigure2 <- function(table_s6, xsize=9, mainsize=12, tsgdf, subtypeTable){
+#'data' should be supp. table S6
+generateFigure2 <- function(data, xsize=9, mainsize=12, subtypeTable){
   colorvec <- c('Amplification'="#D25047",'Mutation'="#CED1CE",'Mutation + Gain of Mutant'="#EDBE59",'Fusion'='plum1','Compound'= "#8CA1D3",'Homdel'="#242860", 
                 'Mut + Fusion'="#9E65AA",'Mut + LOH'="#58B9F4")
-  ###
-  #Removing amplifications in RUNX1, GATA3, and FOXA1:
-  removeAmps <- which(data$Hugo_Symbol %in% c('RUNX1','GATA3','FOXA1') & data$zygosity_call %in% 
-                        c('Amplification - (gene-level)','Amplification - CNA'))
-  if(length(removeAmps) > 0){data <- data[-removeAmps,]}
-  f2 <- biallelicRateTileplot(data=table_s6, subtypeTable=subtypeTable, colorvec=colorvec)
-  
-  #Save figure:
-  pdf(paste(figurePath,'/f2.pdf',sep=''),width = 26.4, height = 20.6)
-  print(f2)
-  dev.off()
+  f2 <- biallelicRateTileplot(data=data, colorvec=colorvec, subtypeTable=subtypeTable)
+  f2
 }
-generateFigure2(table_s6, xsize=10, mainsize=12, tsgdf, subtypeTable=select_ctds)
+generateFigure2(data, subtypeTable=select_ctds, xsize=10, mainsize=12)
 
